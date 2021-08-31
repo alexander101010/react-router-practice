10147fd5ff4802add88511dfb0b7392b9cc2546a
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-31 11:45:07 +0200

    added routes with switch and a redirect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+
+import MainHeader from './components/MainHeader';
+import ProductDetail from './components/ProductDetail';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome' />
+          </Route>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productID'>
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/welcome -> Welcome Component
+// our-domain.com/products -> Product Component
+// our-domain.com/product-detail/{any-value} ->
